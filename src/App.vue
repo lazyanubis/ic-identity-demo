0be760168b0e3b4f834c37e743eef318daeef397
anubis <anubis@icpleague.com>
2023-01-07 19:34:21 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue';
-import VConsole from 'vconsole';
-new VConsole();
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+    <div class="header">
+        <select>
+            <option value="II">II</option>
+        </select>
     </div>
-    <HelloWorld msg="Vite + Vue" />
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="less" scoped>
+.header {
+    > select {
+        width: 500px;
+        height: 28px;
+    }
 }
 </style>
